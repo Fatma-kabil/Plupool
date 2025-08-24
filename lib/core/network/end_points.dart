20149ea8e2f4151d8c46923bdafb27f0c158e32b
Fatma_kabil <fatmakhaled112@gmail.com>
2025-08-24 10:59:33 +0200

    create end points file

---
+++ lib/core/network/end_points.dart
@@ -0,0 +1,7 @@
+class Endpoints {
+//   ربط الباك إند الحقيقي
+static const String baseUrl = 'https://api.example.com';
+static const String loginPhone = '/auth/login-phone';
+static const String signUpPhone = '/auth/signup-phone';
+static const String profile = '/me';
+}
