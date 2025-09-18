1dce27075a4f0210b31a5506a8f3fd67c4865fb5
Fatma_kabil <fatmakhaled112@gmail.com>
2025-09-18 10:12:21 +0200

    finish login view

---
+++ lib/features/auth/presentation/views/widgets/login_form.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:plupool/core/theme/app_text_styles.dart';
+import 'package:plupool/core/utils/validators.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/custom_text_form_field.dart';
+
+class LoginForm extends StatelessWidget {
+  LoginForm({super.key});
+
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          SizedBox(height: 6),
+          // 📧 البريد أو الهاتف
+          Text(
+            'البريد الإلكتروني أو رقم الهاتف',
+            style: AppTextStyles.styleSemiBold16(context)
+                .copyWith(color: const Color(0xff333333)),
+          ),
+          const SizedBox(height: 15),
+          CustomTextFormField(
+            controller: _emailController,
+            hintText: 'أدخل البريد الإلكتروني أو رقم الهاتف',
+            icon: Icons.alternate_email, // أيقونة أنسب
+            keyboardType: TextInputType.emailAddress,
+            validator: (v) => Validators.email(v),
+          ),
+          const SizedBox(height: 6),
+
+          // 🔑 كلمة المرور
+          Text(
+            'كلمة المرور',
+            style: AppTextStyles.styleSemiBold16(context)
+                .copyWith(color: const Color(0xff333333)),
+          ),
+          const SizedBox(height: 15),
+          CustomTextFormField(
+            controller: _passwordController,
+            hintText: 'أدخل كلمة المرور',
+            icon: Icons.lock_outline, // أيقونة أوضح لكلمة المرور
+            obscureText: true,
+            keyboardType: TextInputType.visiblePassword,
+            validator: (v) => Validators.password(v),
+            // TODO: ضيفي أيقونة عرض/إخفاء كلمة المرور في sufficon لو حابة
+          ),
+          const SizedBox(height: 14),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:plupool/core/theme/app_text_styles.dart';
 import 'package:plupool/features/auth/presentation/views/widgets/auth_header.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/auth_switch_row.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/custom_check_box.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/custom_text_btn.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/divider_with_text.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/google_btn.dart';
+import 'package:plupool/features/auth/presentation/views/widgets/login_form.dart';
 
-class LoginViewBody extends StatelessWidget {
+class LoginViewBody extends StatefulWidget {
   const LoginViewBody({super.key});
+
+  @override
+  State<LoginViewBody> createState() => _LoginViewBodyState();
+}
+
+class _LoginViewBodyState extends State<LoginViewBody> {
+  bool acceptedTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +30,59 @@ class LoginViewBody extends StatelessWidget {
               title: 'يلا نسجل دخولك',
               subtitle: 'أدخل بياناتك للوصول لحسابك',
             ),
+            LoginForm(),
+            SizedBox(height: 13),
+            Row(
+              textDirection: TextDirection.rtl,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    ' نسيت كلمة المرور  ',
+                    style: AppTextStyles.styleSemiBold14(context).copyWith(
+                      decoration: TextDecoration.underline,
+                      color: Color(0xff777777),
+                    ),
+                  ),
+                ),
+                Spacer(),
+                CustomCheckbox(
+                  value: acceptedTerms,
+                  onChanged: (val) {
+                    setState(() {
+                      acceptedTerms = val;
+                    });
+                  },
+                  label: 'تذكرني',
+                ),
+              ],
+            ),
+            SizedBox(height: 59),
+            CustomTextBtn(
+              text: ' تسجيل الدخول ',
+              onPressed: () {
+                // إضافة وظيفة الزر هنا
+                //     context.push('/verification');
+              },
+            ),
+            SizedBox(height: 58),
+            DividerWithText(text: 'تسجيل الدخول عن طريق'),
+            SizedBox(height: 40),
+            GoogleBtn(
+              onTap: () {
+                //   context.push('/verification');
+              },
+            ),
+            SizedBox(height: 66),
+
+            AuthSwitchRow(
+              leadingText: ' ليس لدي حساب ',
+              actionText: ' انشاء حساب',
+              onTap: () {
+                context.push('/signup');
+              },
+            ),
+            SizedBox(height: 104,)
           ],
         ),
       ),
