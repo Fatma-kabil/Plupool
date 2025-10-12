f68a91bea13f6ea9835bbb2ba02b33363b8ee152
Fatma_kabil <fatmakhaled112@gmail.com>
2025-10-12 13:31:29 +0200

    finish tech task view

--- lib/features/tasks/presentation/views/widgets/tech_task_view_body.dart
+++ lib/features/tasks/presentation/views/widgets/tech_task_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:plupool/core/constants.dart';
 import 'package:plupool/core/theme/app_colors.dart';
 import 'package:plupool/core/theme/app_text_styles.dart';
+import 'package:plupool/core/utils/size_config.dart';
 import 'package:plupool/core/utils/widgets/filter_button.dart';
+import 'package:plupool/features/tasks/presentation/views/widgets/my_task_view_card.dart';
 
 class TechTaskViewBody extends StatelessWidget {
   const TechTaskViewBody({super.key});
@@ -25,7 +28,19 @@ class TechTaskViewBody extends StatelessWidget {
             },)
           ],
         ),
-        SizedBox(height: 50,)
+        SizedBox(height: 40,),
+        ListView.builder(
+           shrinkWrap: true, // 👈 يمنعها من التمدد اللانهائي
+          physics: const NeverScrollableScrollPhysics(), // 👈 توقف السكرول الداخلي
+      itemCount: requests.length,
+      itemBuilder: (context, index) {
+        
+        return Padding(
+          padding: EdgeInsets.only(bottom: SizeConfig.h(5)),
+          child: MyTaskViewCard(request:requests[index] ),
+        );
+      },
+    )
       ],
     );
   }
