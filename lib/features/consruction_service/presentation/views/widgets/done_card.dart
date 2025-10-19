53b5e9cabad488fe731e20a1f98540e205d0f8fb
Fatma_kabil <fatmakhaled112@gmail.com>
2025-10-19 09:12:14 +0200

    apply responsive to done card

--- lib/features/consruction_service/presentation/views/widgets/done_card.dart
+++ lib/features/consruction_service/presentation/views/widgets/done_card.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:plupool/core/theme/app_colors.dart';
+import 'package:plupool/core/utils/size_config.dart';
 import 'package:plupool/core/utils/widgets/header_text.dart';
 import 'package:plupool/core/utils/widgets/success_image.dart';
 
 class DoneCard extends StatelessWidget {
   const DoneCard({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+     final double cardWidth = SizeConfig.screenWidth * 0.65; // 👈 يخليها 85% من عرض الشاشة
     return Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      insetPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 29),
+      insetPadding:  EdgeInsets.symmetric(horizontal:SizeConfig.w(16) , vertical:SizeConfig.h(29) ),
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 43),
+         width:SizeConfig.isWideScreen? cardWidth:double.infinity,
+        padding:  EdgeInsets.symmetric(horizontal:SizeConfig.w(16) , vertical:SizeConfig.h(43) ),
         decoration: BoxDecoration(
           color: AppColors.kScaffoldColor,
           borderRadius: BorderRadius.circular(20),
