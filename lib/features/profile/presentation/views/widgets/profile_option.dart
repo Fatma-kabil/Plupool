7268e612199c3bc200641291910db593fa96aeba
Fatma_kabil <fatmakhaled112@gmail.com>
2025-11-04 10:25:25 +0200

    create profile option widget

---
+++ lib/features/profile/presentation/views/widgets/profile_option.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:plupool/core/theme/app_colors.dart';
+import 'package:plupool/core/theme/app_text_styles.dart';
+import 'package:plupool/core/utils/size_config.dart';
+
+class ProfileOption extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback onTap;
+  final bool islogout;
+
+  const ProfileOption({
+    required this.title,
+    required this.icon,
+    required this.onTap,
+    this.islogout = false,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: Icon(
+        icon,
+        color:islogout?Colors.red: AppColors.kprimarycolor,
+        size: SizeConfig.w(19),
+      ),
+      title: Text(
+        title,
+        textAlign: TextAlign.right,
+        style: AppTextStyles.styleMedium16(
+          context,
+        ).copyWith(color:islogout?Colors.red: Color(0xff777777)),
+      ),
+      trailing: Icon(
+        Icons.chevron_left,
+        color:islogout?Colors.red: Color(0xffA4A4A4),
+        size: SizeConfig.w(20),
+      ),
+      onTap: onTap,
+    );
+  }
+}
