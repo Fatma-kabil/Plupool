1479572dc4d890ac7ef735d4bc7951084488edb1
Fatma_kabil <fatmakhaled112@gmail.com>
2025-11-13 13:55:23 +0200

    create input fields section

---
+++ lib/features/tasks/presentation/views/widgets/input_fields_section.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:plupool/core/theme/app_colors.dart';
+import 'package:plupool/core/theme/app_text_styles.dart';
+import 'package:plupool/core/utils/size_config.dart';
+import 'package:plupool/core/utils/widgets/custom_text_form_field.dart';
+
+class InputFieldsSection extends StatelessWidget {
+  const InputFieldsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "مستوى الكلور",
+            style: AppTextStyles.styleSemiBold16(
+              context,
+            ).copyWith(color: AppColors.ktextcolor),
+          ),
+          SizedBox(height: SizeConfig.h(4)),
+
+          CustomTextFormField(icon: Icons.science, hintText: "2.6 ppm"),
+          Padding(
+            padding: EdgeInsets.only(top: SizeConfig.h(4)),
+            child: Text(
+              "المستوى الموصى به 1.0 - 3.0 جزء في المليون",
+              style: AppTextStyles.styleRegular13(
+                context,
+              ).copyWith(color: Color(0xffBBBBBB)),
+              textAlign: TextAlign.right,
+            ),
+          ),
+
+          SizedBox(height: SizeConfig.h(12)),
+          Text(
+            "مستوى الحموضة",
+            style: AppTextStyles.styleSemiBold16(
+              context,
+            ).copyWith(color: AppColors.ktextcolor),
+          ),
+          SizedBox(height: SizeConfig.h(4)),
+
+          CustomTextFormField(icon: Icons.water_drop, hintText: "2.7"),
+          Padding(
+            padding: EdgeInsets.only(top: SizeConfig.h(4)),
+            child: Text(
+              "المستوى الموصى به 7.2 - 7.6",
+              style: AppTextStyles.styleRegular13(
+                context,
+              ).copyWith(color: Color(0xffBBBBBB)),
+              textAlign: TextAlign.right,
+            ),
+          ),
+
+          SizedBox(height: SizeConfig.h(12)),
+          Text(
+            "درجة الحرارة",
+            style: AppTextStyles.styleSemiBold16(
+              context,
+            ).copyWith(color: AppColors.ktextcolor),
+          ),
+          SizedBox(height: SizeConfig.h(4)),
+
+          CustomTextFormField(icon: Icons.thermostat, hintText: "25°"),
+
+          SizedBox(height: SizeConfig.h(12)),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/tasks/presentation/views/widgets/maintenance_update_view_body.dart
+++ lib/features/tasks/presentation/views/widgets/maintenance_update_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:plupool/core/utils/size_config.dart';
+import 'package:plupool/features/tasks/presentation/views/widgets/input_fields_section.dart';
 import 'package:plupool/features/tasks/presentation/views/widgets/previous_value_section.dart';
 
 class MaintenanceUpdateViewBody extends StatelessWidget {
@@ -12,6 +13,7 @@ class MaintenanceUpdateViewBody extends StatelessWidget {
         children: [
          PreviousValuesSection(),
          SizedBox(height: SizeConfig.h(20)),
+         InputFieldsSection(),
           // أضف المزيد من عناصر واجهة المستخدم حسب الحاجة
         ],
       ),
