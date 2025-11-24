f1d58e0ebd3998f03d1b9118fb8f9c2d8aeee56c
Fatma_kabil <fatmakhaled112@gmail.com>
2025-11-24 11:28:23 +0200

    create user cubit

---
+++ lib/features/profile/presentation/manager/user_cubit/user_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:plupool/features/profile/domain/repos/user_repo.dart';
+import 'user_state.dart';
+
+class UserCubit extends Cubit<UserState> {
+  final UserRepository userRepository;
+
+  UserCubit(this.userRepository) : super(UserInitial());
+
+  Future<void> fetchCurrentUser(String token) async {
+    emit(UserLoading());
+
+    final failureOrUser = await userRepository.getCurrentUser(token);
+
+    failureOrUser.fold(
+      (failure) => emit(UserError(failure.message)),
+      (user) => emit(UserLoaded(user)),
+    );
+  }
+}
