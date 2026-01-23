0ce177dbb0b2850b093d541ca097af8891842fc8
Fatma_kabil <fatmakhaled112@gmail.com>
2026-01-23 19:40:40 +0200

    create edit note card

--- lib/features/notes/presentation/views/widgets/admin_note_card.dart
+++ lib/features/notes/presentation/views/widgets/admin_note_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plupool/core/theme/app_colors.dart';
 import 'package:plupool/core/utils/size_config.dart';
 import 'package:plupool/features/notes/presentation/views/widgets/date_time_notes.dart';
+import 'package:plupool/features/notes/presentation/views/widgets/edit_note_card.dart';
 import 'package:plupool/features/notes/presentation/views/widgets/notes_row.dart';
 import 'package:plupool/features/support/presentation/views/widgets/attachment_chip.dart';
 
@@ -43,10 +44,30 @@ class AdminNoteCard extends StatelessWidget {
                 ),
               ),
               SizedBox(width: SizeConfig.w(8)),
-              Icon(
-                Icons.edit_document,
-                size: SizeConfig.w(20),
-                color: const Color(0xff0077B6),
+              GestureDetector(
+                onTap: () {
+                  Dialog(
+                    backgroundColor: AppColors.kScaffoldColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    insetPadding: EdgeInsets.symmetric(
+                      horizontal: SizeConfig.w(20),
+                      vertical: SizeConfig.h(29),
+                    ),
+                    child: EditNoteCard(
+                      noteController: TextEditingController(
+                        text:
+                            'العميل يفضل الخدمات في الصباح الباكر (قبل ٩ صباحاً). لديه مسبح كبير بحجم 10×5 متر.  وهو عميل متميز  للشركة منذ أكثر من سنتين.',
+                      ),
+                    ),
+                  );
+                },
+                child: Icon(
+                  Icons.edit_document,
+                  size: SizeConfig.w(20),
+                  color: const Color(0xff0077B6),
+                ),
               ),
               SizedBox(width: SizeConfig.w(15)),
               Icon(
---
+++ lib/features/notes/presentation/views/widgets/edit_note_card.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:plupool/core/theme/app_text_styles.dart';
+import 'package:plupool/core/utils/size_config.dart';
+import 'package:plupool/core/utils/widgets/custom_text_btn.dart';
+import 'package:plupool/features/home/presentaation/views/customer/widgets/note_field.dart';
+import 'package:plupool/features/support/presentation/views/widgets/attachment_chip.dart';
+
+// ignore: must_be_immutable
+class EditNoteCard extends StatelessWidget {
+  EditNoteCard({super.key, required this.noteController});
+
+  final _formKey = GlobalKey<FormState>(); // ✅ مفتاح للفورم
+  final TextEditingController noteController;
+  bool notesfiles = true;
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  "الملاحظات",
+                  style: AppTextStyles.styleSemiBold16(context),
+                ),
+              ),
+              const SizedBox(height: 8),
+              NoteField(controller: noteController),
+              SizedBox(height: 15),
+              notesfiles
+                  ? SizedBox(
+                      height: SizeConfig.h(30),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 2,
+                        itemBuilder: (context, index) {
+                          return AttachmentChip(fileName: "file.txt");
+                        },
+                      ),
+                    )
+                  : Text(
+                      'لا توجد',
+                      style: AppTextStyles.styleRegular16(
+                        context,
+                      ).copyWith(color: Color(0xff777777)),
+                    ),
+
+              // ✅ هيشتغل
+              const SizedBox(height: 40),
+              CustomTextBtn(
+                width: double.infinity,
+                text: 'حفظ',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
