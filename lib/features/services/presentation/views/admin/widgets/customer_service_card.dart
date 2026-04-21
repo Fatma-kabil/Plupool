bf5281ff0bdd488464fd164645fdff35d4c2fbbb
Fatma_kabil <fatmakhaled112@gmail.com>
2026-04-22 00:07:00 +0200

    update code

--- lib/features/services/presentation/views/admin/widgets/customer_service_card.dart
+++ lib/features/services/presentation/views/admin/widgets/customer_service_card.dart
@@ -6,14 +6,18 @@ import 'package:plupool/core/theme/app_text_styles.dart';
 import 'package:plupool/core/utils/functions/build_statue_label.dart';
 import 'package:plupool/core/utils/size_config.dart';
 import 'package:plupool/core/utils/functions/request_status.dart';
-import 'package:plupool/features/services/data/models/customer_service_card_model.dart';
+import 'package:plupool/core/utils/widgets/show_custom_snackbar.dart';
+import 'package:plupool/features/orders/presentation/view/widgets/delete_order_card.dart';
+import 'package:plupool/features/services/domain/entities/booking_entity.dart';
+import 'package:plupool/features/services/presentation/manager/cubits/booking_cubit.dart';
+import 'package:plupool/features/services/presentation/manager/cubits/booking_state.dart';
 import 'package:plupool/features/services/presentation/views/admin/widgets/date_row.dart';
 import 'package:plupool/features/services/presentation/views/admin/widgets/service_card_footer.dart';
 import 'package:plupool/features/services/presentation/views/admin/widgets/service_card_row.dart';
 
 class CustomerServiceCard extends StatelessWidget {
-  const CustomerServiceCard({super.key, required this.request});
-  final CustomerServiceCardModel request;
+  const CustomerServiceCard({super.key, required this.booking});
+  final BookingEntity booking;
 
   @override
   Widget build(BuildContext context) {
@@ -54,8 +58,8 @@ class CustomerServiceCard extends StatelessWidget {
                     buildStatusLabel(colors, context, status),
                   ],
                 ),
-             //   SizedBox(height: 8),
-                DateRow(date: request.date),
+                //   SizedBox(height: 8),
+                DateRow(date: booking.date),
               ],
             ),
 
@@ -63,55 +67,58 @@ class CustomerServiceCard extends StatelessWidget {
 
             ServiceCardRow(title: "نوع الخدمة:", value: booking.bookingType),
             const SizedBox(height: 5),
-             ServiceCardRow(title: " العميل:", value: "احمد محمد"),
+            ServiceCardRow(title: " العميل:", value: booking.userName),
             const SizedBox(height: 5),
-            ServiceCardRow(title: "الفنيين:", value: request.techs),
+            ServiceCardRow(
+              title: "الفنيين:",
+              value: booking.technicians.join(" - "),
+            ),
             const SizedBox(height: 8),
-            ServiceCardFooter(delFun: () {
-               showDialog(
-                    context: context,
-                    builder: (dialogContext) {
-                       final cubit = context.read<BookingCubit>();
-                      return BlocConsumer<BookingCubit, BookingState>(
-                           bloc: cubit, 
-                        listener: (context, state) {
-                          if (state is BookingDeleted) {
-                            Navigator.pop(context);
+            ServiceCardFooter(
+              delFun: () {
+                showDialog(
+                  context: context,
+                  builder: (dialogContext) {
+                    final cubit = context.read<BookingCubit>();
+                    return BlocConsumer<BookingCubit, BookingState>(
+                      bloc: cubit,
+                      listener: (context, state) {
+                        if (state is BookingDeleted) {
+                          Navigator.pop(context);
 
-                            showCustomSnackBar(
-                              context: context,
-                              message: "تم حذف الطلب بنجاح 🗑️",
-                              isSuccess: true,
-                            );
-                          }
-
-                          if (state is BookingError) {
-                            Navigator.pop(context);
-
-                            showCustomSnackBar(
-                              context: context,
-                              message: state.message,
-                            );
-                          }
-                        },
-                        builder: (context, state) {
-                        
-                          final isLoading = state is BookingLoading;
-
-                          return DeleteOrderCard(
-                            text: "هل أنت متأكد من حذف هذا الطلب؟",
-                            isLoading: isLoading,
-                            onPressed: isLoading
-                                ? null
-                                : () {
-                                    cubit.deleteBooking(booking.id);
-                                  },
+                          showCustomSnackBar(
+                            context: context,
+                            message: "تم حذف الطلب بنجاح 🗑️",
+                            isSuccess: true,
                           );
-                        },
-                      );
-                    },
-                  );
-                },
+                        }
+
+                        if (state is BookingError) {
+                          Navigator.pop(context);
+
+                          showCustomSnackBar(
+                            context: context,
+                            message: state.message,
+                          );
+                        }
+                      },
+                      builder: (context, state) {
+                        final isLoading = state is BookingLoading;
+
+                        return DeleteOrderCard(
+                          text: "هل أنت متأكد من حذف هذا الطلب؟",
+                          isLoading: isLoading,
+                          onPressed: isLoading
+                              ? null
+                              : () {
+                                  cubit.deleteBooking(booking.id);
+                                },
+                        );
+                      },
+                    );
+                  },
+                );
+              },
             ),
             // ---- بيانات المستخدم + زرار الموقع ----
           ],
